a7b866265e0023bafb05048c10707066915e8f4a
Валерий Ишутин <vishutin@yandex.ru>
2023-06-16 22:14:21 +0300

    Fixed errors2

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -20,4 +20,3 @@ class TaskiAPITestCase(TestCase):
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
-        
